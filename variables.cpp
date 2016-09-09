0f317aa1b6f61f3ec3b0f175ba64e3007e1ed922
Slackware Live User <live@darkstar.example.net>
2016-09-09 22:55:25 +0000

    poerando con variables

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+// operando con variables
+
+#include <iostream>
+
+int main()
+{
+  // declarando variables
+  int a, b;
+  int result;
+
+  // proceso
+  a = 5;
+  b = 2;
+  a = a + 1;
+  result = a - b;
+
+  std::cout <<result<< std::endl;
+
+  return 0;
+}
